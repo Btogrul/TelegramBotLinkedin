22051712a46ddd8e1806dd3a8f1f82b21e5805e1
Btogrul <baghirli.togrul@gmail.com>
2024-04-05 15:08:19 +0400

    errors handle

--- src/main/java/com/ltc/telegrambotlinkedin/controller/JSearchController.java
+++ src/main/java/com/ltc/telegrambotlinkedin/controller/JSearchController.java
@@ -1,20 +1,20 @@
 package com.ltc.telegrambotlinkedin.controller;
 
 import com.ltc.telegrambotlinkedin.config.feign.JSearchClient;
-import com.ltc.telegrambotlinkedin.dto.MessageRoot;
 import com.ltc.telegrambotlinkedin.dto.jSearchDto.JSearchRoot;
-import com.ltc.telegrambotlinkedin.dto.jSearchDto.Parameters;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/job")
 public class JSearchController {
-private final JSearchClient jSearchClient;
+    private final JSearchClient jSearchClient;
     private final String jSearchKey = "a005368adfmsh9475ffec9ce47d5p19cc0cjsn3af963af6e73";
     private final String jSearchHost = "jsearch.p.rapidapi.com";
-
 
 
     @GetMapping
