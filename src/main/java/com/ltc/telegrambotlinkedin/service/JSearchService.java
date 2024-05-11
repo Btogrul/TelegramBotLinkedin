e488b0edc1d29a4999d49d10b04387f3fb092c7d
Nicat Qurbanov <nicat.qurbanov97@gmail.com>
2024-05-11 15:27:08 +0400

    Spring Boot i18n is added but is incomplete

--- src/main/java/com/ltc/telegrambotlinkedin/service/JSearchService.java
+++ src/main/java/com/ltc/telegrambotlinkedin/service/JSearchService.java
@@ -5,7 +5,6 @@ import com.ltc.telegrambotlinkedin.dto.jSearchDto.Job;
 import com.ltc.telegrambotlinkedin.entity.UserOfBot;
 import com.ltc.telegrambotlinkedin.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
