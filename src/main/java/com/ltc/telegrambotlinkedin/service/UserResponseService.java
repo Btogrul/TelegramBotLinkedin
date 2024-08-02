1c569fee1a20c21ac543c606197b240406960037
Nicat Qurbanov <nicat.qurbanov97@gmail.com>
2024-08-02 17:52:26 +0400

    Minor changes

--- src/main/java/com/ltc/telegrambotlinkedin/service/UserResponseService.java
+++ src/main/java/com/ltc/telegrambotlinkedin/service/UserResponseService.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class TelegramBotResponseService {
+public class UserResponseService {
 
     private final UserRepository userRepo;
     private final JSearchService jSearchService;
